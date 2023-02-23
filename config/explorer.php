fcdf94ab13ef35563332f3c7caa5058820664776
Surajit Basak <surajit@adrobit.com>
2023-02-23 19:15:10 +0530

    add conditional configuration for elastic search

--- config/explorer.php
+++ config/explorer.php
@@ -2,22 +2,29 @@
 
 declare(strict_types=1);
 
+$connection = env('APP_ENV') == 'production' ? [
+    'elasticCloudId' => env('ELASTIC_CLOUD_ID', null),
+    'auth' => [
+        'username' => env('ELASTIC_USERNAME', 'elastic'),
+        'password' => env('ELASTIC_PASSWORD')
+    ]
+]: [
+    'host' => 'localhost',
+    'port' => '9200',
+    'scheme' => 'http',
+    'auth' => [
+        'username' => env('ELASTIC_USERNAME', 'elastic'),
+        'password' => env('ELASTIC_PASSWORD')
+    ]
+];
+
 return [
     /*
      * There are different options for the connection. Since Explorer uses the Elasticsearch PHP SDK
      * under the hood, all the host configuration options of the SDK are applicable here. See
      * https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/configuration.html
      */
-    'connection' => [
-        'host' => 'localhost',
-        'port' => '9200',
-        'scheme' => 'http',
-        // 'elasticCloudId' => env('ELASTIC_CLOUD_ID', null),
-        'auth' => [
-            'username' => env('ELASTIC_USERNAME', 'elastic'),
-            'password' => env('ELASTIC_PASSWORD')
-        ],
-    ],
+    'connection' => $connection,
 
     /**
      * An index may be defined on an Eloquent model or inline below. A more in depth explanation
